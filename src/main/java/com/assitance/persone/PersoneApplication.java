d1720ce8765efe285dfbf1adc504771a379d61c3
Zeineb_BenJeddou <zeinebbenjeddou01@gmail.com>
2025-03-15 23:57:22 +0100

    first commit

---
+++ src/main/java/com/assitance/persone/PersoneApplication.java
@@ -0,0 +1,13 @@
+package com.assitance.persone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/assitance/persone/PersoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assitance.persone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
